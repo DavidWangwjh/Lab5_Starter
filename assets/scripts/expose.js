6b92ae761b36133ceeb1e9eafb6eca6cd24de39e
DavidWangwjh <davidwang.wjh@gmail.com>
2022-10-30 16:19:19 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -16,19 +16,19 @@ function init() {
   horn.addEventListener('change', (e) => {
     hornSelected = true
     if (e.target.value == 'air-horn'){
-      hornImage.src = '../assets/images/air-horn.svg';
+      hornImage.src = 'assets/images/air-horn.svg';
       hornImage.alt = 'Air horn image selected';
-      audio.src = '../assets/audio/air-horn.mp3';
+      audio.src = 'assets/audio/air-horn.mp3';
       showConfetti = false;
     } else if (e.target.value == 'car-horn'){
-      hornImage.src = '../assets/images/car-horn.svg';
+      hornImage.src = 'assets/images/car-horn.svg';
       hornImage.alt = 'Car horn image selected';
-      audio.src = '../assets/audio/car-horn.mp3';
+      audio.src = 'assets/audio/car-horn.mp3';
       showConfetti = false;
     } else if (e.target.value == 'party-horn'){
-      hornImage.src = '../assets/images/party-horn.svg';
+      hornImage.src = 'assets/images/party-horn.svg';
       hornImage.alt = 'Party horn image selected';
-      audio.src = '../assets/audio/party-horn.mp3';
+      audio.src = 'assets/audio/party-horn.mp3';
       showConfetti = true;
     }
   });
@@ -36,16 +36,16 @@ function init() {
   volume.addEventListener('change', (e) => {
     const image = document.querySelector('div img');
     if (e.target.value == 0){
-      image.src = '../assets/icons/volume-level-0.svg';
+      image.src = 'assets/icons/volume-level-0.svg';
       image.alt = 'Volume level 0';
     } else if (e.target.value < 33){
-      image.src = '../assets/icons/volume-level-1.svg';
+      image.src = 'assets/icons/volume-level-1.svg';
       image.alt = 'Volume level 1';
     } else if (e.target.value < 67){
-      image.src = '../assets/icons/volume-level-2.svg';
+      image.src = 'assets/icons/volume-level-2.svg';
       image.alt = 'Volume level 2';
     } else {
-      image.src = '../assets/icons/volume-level-3.svg';
+      image.src = 'assets/icons/volume-level-3.svg';
       image.alt = 'Volume level 3';
     }
     audio.volume = e.target.value/100;
